f62a40c4920d9b4c6ee5992857263e7c76a0f714
Samim <samimsafaie@gmail.com>
2022-02-14 13:13:37 +0330

    icon added

--- src/services/movieService.js
+++
@@ -1,9 +0,0 @@
-import http from "./httpService";
-
-import config from "./config.json";
-
-export const addMovie = movie => {
-    return http.post(`${config.localapi}/index.php/create`,
-        JSON.stringify(movie)
-    );
-};
